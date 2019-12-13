b62b942db8031d44ffc1c5d0e6b1e8fa806192b3
unknown <kumarnigam28@gmail.com>
2019-12-13 16:57:34 +0530

    Responsive last

--- src/components/aboutpage/section1/index.js
+++ src/components/aboutpage/section1/index.js
@@ -13,10 +13,10 @@ export default  class Section1About extends React.Component {
       	<Grid>
           <div className="overlay"></div>
       		<Row>
-      			<Col lg={12} md={12} sm={6} xs={12}>
+      			<Col lg={12} md={12} sm={12} xs={12}>
       				<Image src={pic} responsive/>
       			</Col>
-      		  <Col lg={10} md={12} sm={6} xs={12} lgOffset={1}>
+      		  <Col lg={10} md={12} sm={12} xs={12} lgOffset={1}>
       		    <h1>Our mission is to solve climate change by scaling clean energy to 1 billion people.</h1>
       		    <p>As companies expand, their carbon footprint and environmental impact grows. The demand for clean energy projects to mitigate this increasing footprint is soaring. Our suite of digital solutions engages companies of any size to purchase radically more clean energy and make the world cleaner.</p>
       		  </Col>
