7d6618f7ac27fd31af9054f1f1f5c12ef7f14db2
unknown <kumarnigam28@gmail.com>
2019-12-13 16:27:32 +0530

    Responsive...

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 import PageHeader from '../common/header/';
 
-import Section1Home from './section1/';
-import Section2Home from './section2/';
-import Section3Home from './section3/';
-import Section4Home from './section4/';
-import Section5Home from './section5/';
+//import Section1Home from './section1/';
+//import Section2Home from './section2/';
+//import Section3Home from './section3/';
+//import Section4Home from './section4/';
+//import Section5Home from './section5/';
 
 import Section6Home from './section6/';
 
@@ -27,11 +27,11 @@ export default class HomePageContainer extends Component {
 			<React.Fragment>
 					<PageHeader />
 
-					<Section1Home/>
+					{/*<Section1Home/>
 		      <Section2Home/>
 		      <Section3Home/>
 		      <Section4Home/>
-		      <Section5Home/>
+		      <Section5Home/>*/}
 
 		      <Section1About/>
 		      <Section2About/>
