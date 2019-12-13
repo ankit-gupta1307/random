c00e1a93f6dd10dae25e888981d04d095cb30c97
unknown <kumarnigam28@gmail.com>
2019-12-13 17:26:59 +0530

    Responsive...

--- src/components/homepage/section5/index.js
+++ src/components/homepage/section5/index.js
@@ -11,12 +11,12 @@ export default  class Section5Home extends React.Component {
       <section className="sectionAlign sectionLast gray text-center">
       	<Grid>
       		<Row>
-      		  <Col lg={6}>
+      		  <Col lg={6} md={6} sm={6} xs={12}>
       		    <h1>Go Green with Cue</h1>
       		    <p>We’ve built the most sophisticated and connected renewable network to date.</p>
       		    <Button className="headerButton textBlack inline-block">Get Que</Button>
       		  </Col>
-      		  <Col lg={6}>
+      		  <Col lg={6} md={6} sm={6} xs={12}>
       		    <h1>Develop Projects with View</h1>
       		    <p>Our flagship product accelerates clean energy development worldwide.</p>
       		    <Button className="headerButton textBlack inline-block">Get View</Button>
