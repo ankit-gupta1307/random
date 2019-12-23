79e4484f42bcc9c54c581a81a10203b994636616
Ankit Gupta <ank92gwalior@gmail.com>
2019-12-23 13:00:06 +0530

    Router added

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -7,15 +7,9 @@ import Section2Home from './section2/';
 import Section3Home from './section3/';
 import Section4Home from './section4/';
 import Section5Home from './section5/';
-
 import Section6Home from './section6/';
 
 
-import Section1About from '../aboutpage/section1/';
-import Section2About from '../aboutpage/section2/';
-import Section3About from '../aboutpage/section3/';
-import Section4About from '../aboutpage/section4/';
-import Section5About from '../aboutpage/section5/';
 
 
 
@@ -33,11 +27,7 @@ export default class HomePageContainer extends Component {
 		      <Section4Home/>
 		      <Section5Home/>
 
-		      <Section1About/>
-		      <Section2About/>
-		      <Section3About/>
-		      <Section4About/>
-		      <Section5About/>
+		      
 
 		      <Section6Home/>
 			</React.Fragment>
