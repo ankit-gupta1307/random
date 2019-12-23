94a3611e536007b2eca49542ac053dda7cd94e48
Ankit Gupta <ank92gwalior@gmail.com>
2019-12-23 12:52:19 +0530

    Updates

---
+++ precache-manifest.ab278c3208c9cdb610c3f480cb8f52b5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ccb43d11a5d82dbe74992e1d62ce87",
+    "url": "/random/index.html"
+  },
+  {
+    "revision": "7e873489ba1c3652b5fd",
+    "url": "/random/static/css/main.02cf9204.chunk.css"
+  },
+  {
+    "revision": "adc5cc2283c9d5827220",
+    "url": "/random/static/js/2.433caf20.chunk.js"
+  },
+  {
+    "revision": "2fbf99eb7b46913e26be09ed5a76f580",
+    "url": "/random/static/js/2.433caf20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e873489ba1c3652b5fd",
+    "url": "/random/static/js/main.42c43e1c.chunk.js"
+  },
+  {
+    "revision": "b479c2825692355ad60f",
+    "url": "/random/static/js/runtime-main.2fd274ce.js"
+  },
+  {
+    "revision": "a15258a71bcff8eb791ad54f27144d48",
+    "url": "/random/static/media/FuturaPTLight.a15258a7.otf"
+  },
+  {
+    "revision": "82af4e81f139befeb3b046549f1933ef",
+    "url": "/random/static/media/aboutUsBanner.82af4e81.png"
+  },
+  {
+    "revision": "7d38fb3b9c6a2471b5ea3c923511463d",
+    "url": "/random/static/media/bgAboutBoy.7d38fb3b.jpg"
+  },
+  {
+    "revision": "3df9b3e9455158e7d6fa23b6e720027c",
+    "url": "/random/static/media/bgAboutGirl.3df9b3e9.jpg"
+  },
+  {
+    "revision": "6b7066890ed54927ca4613c277a92c72",
+    "url": "/random/static/media/bgBoy.6b706689.jpg"
+  },
+  {
+    "revision": "53f647e568f4843e90ea45fcfb306d6c",
+    "url": "/random/static/media/icon03.53f647e5.png"
+  },
+  {
+    "revision": "453de628d38895e03938979c9ce4f655",
+    "url": "/random/static/media/img03.453de628.png"
+  },
+  {
+    "revision": "3e5062a9f33c9bc450eaac174ae1d5e5",
+    "url": "/random/static/media/img24x7.3e5062a9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68315b9672c716df1a5299ac473f5d73",
+    "url": "/random/static/media/partner01.68315b96.png"
+  },
+  {
+    "revision": "236d6944e3ec600a495d834ae9889ee2",
+    "url": "/random/static/media/partner02.236d6944.png"
+  },
+  {
+    "revision": "393472fe71c3ef8b54f8e3c87397508a",
+    "url": "/random/static/media/partner03.393472fe.png"
+  },
+  {
+    "revision": "353ec196994165f5b6af45e12ccb4ccd",
+    "url": "/random/static/media/partner05.353ec196.png"
+  },
+  {
+    "revision": "aaabe6111eeec19186a2006f131a19c7",
+    "url": "/random/static/media/partner06.aaabe611.png"
+  }
+]);
